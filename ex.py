3338f2c93e142441112147e9ad878fc0d42a383d
Ericlecio <ericleciojr14@gmail.com>
2023-04-16 23:21:37 -0300

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,59 @@
+filmes = {
+
+"O Poderoso Chefão": {
+
+    "ano": 1972,
+
+    "diretor": "Francis Ford Coppola",
+
+    "atores": ["Marlon Brando", "Al Pacino", "James Caan"]
+
+},
+
+
+"O Senhor dos Anéis": {
+
+    "ano": 2001,
+
+    "diretor": "Peter Jackson",
+
+    "atores": ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"]
+
+},
+
+
+"Titanic": {
+
+    "ano": 1997,
+
+    "diretor": "James Cameron",
+
+    "atores": ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane"]
+
+}
+}
+
+filmes["Matrix"] = {
+
+"ano": 1999,
+
+"diretor": "The Wachowski Brothers",
+
+"atores": ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"]
+}
+
+# filmes['Titanic']['ano'] = 1989
+
+# print(filmes['O Poderoso Chefão']['diretor'])
+# print(filmes["O Senhor dos Anéis"]['atores'])
+# print(filmes.keys())
+
+a = (1,2,3,4,5)
+b =(3,4,5,6,7)
+c = a+b
+print(set(c))
+print(c[2])
+
+d = list(b)
+d.sort(reverse=True)
+print(d)
